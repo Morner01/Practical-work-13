98117a3f855fe276be93db93621ba01f4db924f7
Morner01 <van.maltsev2005@yandex.ru>
2025-12-04 17:26:45 +0300

    v1

---
+++ financial_calc/calculator.py
@@ -0,0 +1,19 @@
+def calculate_simple_interest(principal, rate, time):
+    if principal < 0 or rate < 0 or time < 0:
+        return ValueError("Аргументы должны быть неотрицательными")
+    else:
+        return principal * rate * time / 100
+
+def calculate_compound_interest(principal, rate, time, n=1):
+    if principal < 0 or rate < 0 or time < 0:
+        return ValueError("Аргументы должны быть неотрицательными")
+    if type(n) != int or n < 0:
+        return ValueError("n должно быть целым положительным числом")
+    else:
+        return principal * (1 + rate/(100*n))**(n*time)
+
+def calculate_tax(amount, tax_rate):
+    if tax_rate < 0 or tax_rate > 100:
+        return ValueError("Значение tax_rate не между 0 и 100")
+    else:
+        return amount * tax_rate / 100
---
+++ financial_calc/tests/test_calculator.py
@@ -0,0 +1,23 @@
+import pytest
+
+from calculator import calculate_simple_interest, calculate_compound_interest, calculate_tax
+
+def test_calculate_simple_interest():
+    assert calculate_simple_interest(2000, 5, 10) == 1000.0
+    assert calculate_simple_interest(0, 10, 5) == 0.0
+    with pytest.raises(ValueError):
+        calculate_simple_interest(-2000, 5, 10)
+
+def test_calculate_compound_interest():
+    assert calculate_compound_interest(2000, 5, 10, 2) == 3277.2328805807883
+    assert calculate_compound_interest(2000, 0, 10, 2) == 2000.0
+    with pytest.raises(ValueError):
+        calculate_compound_interest(-2000, 0, 10, 2)
+
+def test_calculate_tax():
+    assert calculate_tax(100, 50) == 50.0
+    assert calculate_tax(0, 30) == 0.0
+    with pytest.raises(ValueError):
+        calculate_tax(1, 101)
+    with pytest.raises(ValueError):
+        calculate_tax(1, -1)
